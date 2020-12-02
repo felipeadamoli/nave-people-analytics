fde03ea2db5c189db0e3ef0ff791005f07f297ed
Felipe Adamoli <felipeadamoli@Felipes-MacBook-Pro.local>
2020-12-02 09:03:22 -0300

    using pandas in the right way

--- reportGenerator.py
+++ reportGenerator.py
@@ -1,16 +1,26 @@
 import pandas as pd
+import numpy as np
 from functions import calcPercentage
 from functions import printLedAndLeader
 from functions import printAnalyze
 from functions import printTotalAndPercentage
 
-df = pd.read_csv('sentimentos.csv')
-# df = pd.read_csv('sentimentos_old.csv')
 
+# df = pd.read_csv('sentimentos_old.csv')
+df = pd.read_csv('sentimentos.csv')
 totalRowCount = len(df.index)
-negativeFeelings = 0
-withoutComments = 0
-withoutLeaderComments = 0
+
+## Analise de quantidade de respostas
+withoutPulse = df[df['Classificacao do sentimento'].isnull()]
+withPulse = df[np.logical_not(df['Classificacao do sentimento'].isnull())]
+withPulseRowCount = len(withPulse.index)
+
+## Analise existencia de tags
+rowsWithoutTags = withPulse[withPulse['Tags do sentimento'].isnull()]
+
+## Analise existencia de comentarios
+rowsWithoutComments = withPulse[withPulse['Comentarios'].isnull()]
+rowsWithComments = withPulse[np.logical_not(withPulse['Comentarios'].isnull())]
 
 print('Fequencia sentimentos')
 print(df['Classificacao do sentimento'].value_counts())
@@ -18,27 +28,28 @@ print(df['Classificacao do sentimento'].value_counts())
 print('\n\n\nPorcentagem sentimentos')
 print(df['Classificacao do sentimento'].value_counts(normalize=True))
 
-
 print('\n\n\nMEMBROS COM SENTIMENTOS NEGATIVOS')
-for index, row in df.iterrows():
-    if isinstance(row['Classificacao do sentimento'], str): 
-        if row['Classificacao do sentimento'] == 'Negativo':
-            negativeFeelings = negativeFeelings + 1
-            printLedAndLeader(row)
-printTotalAndPercentage(negativeFeelings, totalRowCount)
+print(df[df['Classificacao do sentimento'] == 'Negativo'][["Nome", "Nome do Lider"]])
 
-printAnalyze('MEMBROS QUE NAO RESPONDERAM AO SENTIMENTO DA SEMANA', df, 'Classificacao do sentimento')
+print('\n\n\nMEMBROS QUE NAO RESPONDERAM AO SENTIMENTO DA SEMANA')
+print(withoutPulse[["Nome", "Nome do Lider"]])
+print(printTotalAndPercentage(len(withoutPulse.index), totalRowCount))
 
-printAnalyze('RESPOSTAS SEM TAGS', df, 'Tags do sentimento')
+print('\n\n\nRESPOSTAS SEM TAGS')
+print(rowsWithoutTags[["Nome", "Nome do Lider"]])
+print(printTotalAndPercentage(len(rowsWithoutTags.index), withPulseRowCount))
 
-printAnalyze('RESPOSTAS SEM COMENTARIOS', df, 'Comentarios')
+print('\n\n\nRESPOSTAS SEM COMENTARIOS')
+print(rowsWithoutComments[["Nome", "Nome do Lider"]])
+print(printTotalAndPercentage(len(rowsWithoutComments.index), withPulseRowCount))
 
 print('\n\n\nSENTIMENTOS COM COMENTARIOS POREM SEM RESPOSTA DO LIDER')
+withoutLeaderComments = 0
 for index, row in df.iterrows():
     if isinstance(row['Comentarios'], str):
         if isinstance(row['Nome do Lider'], str): 
             if row['Nome do Lider'] not in row['Comentarios']:
                 withoutLeaderComments = withoutLeaderComments + 1
                 printLedAndLeader(row)
-printTotalAndPercentage(withoutLeaderComments, totalRowCount - withoutComments)
+printTotalAndPercentage(withoutLeaderComments, len(rowsWithComments.index))
 print("\n\n\n")
